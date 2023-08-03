67cf49b4c3b936ac59cba101a6f0c0b680522c78
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2023-08-03 03:51:06 -0300

    feat: error handling

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,15 +5,49 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter }
 import { Input } from "./ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
-import { useChat } from "ai/react";
+import { Message, useChat } from "ai/react";
 import useTheme from "@/hooks/theme";
 import { Sun, Moon } from 'lucide-react';
+import { FormEvent, useEffect, useState } from "react";
+import { v4 } from "uuid";
+import cx from 'classnames'
 
 export function Chat() {
-    const { messages, input, handleInputChange, handleSubmit, isLoading } = useChat({
+    const { messages, input, handleInputChange, handleSubmit, isLoading, error } = useChat({
         api: '/api/chat'
     })
     const { theme, toggleTheme } = useTheme();
+
+    const [customMessages, setCustomMessages] = useState<Message[]>([]);
+
+    function sendMessage(event: FormEvent<HTMLFormElement>) {
+        setCustomMessages([
+            ...customMessages,
+            {
+                id: v4(),
+                role: 'user',
+                content: input,
+            }
+        ]);
+        handleSubmit(event);
+    }
+
+    function handleError(error: Error) {
+        const errorObject = JSON.parse(error.message).error;
+
+        setCustomMessages([
+            ...customMessages,
+            {
+                id: v4(),
+                role: 'assistant',
+                content: errorObject.message || 'Something went wrong.',
+            }
+        ]);
+    }
+
+    useEffect(() => {
+        if(error) handleError(error);
+    }, [error])
     
     return (
       <Card className="w-[700px] h-[800px] max-h-[calc(100vh-20px)] max-w-[calc(100vw-20px)] grid grid-rows-[min-content_1fr_min-content]">
@@ -28,9 +62,10 @@ export function Chat() {
         </CardHeader>
 
         <CardContent className="space-y-4 overflow-scroll">
-          {messages.map(message => {
+          {customMessages.map(message => {
             const isUser = message.role === 'user'
             const isAssistant = message.role === 'assistant'
+            const isError = error && message.role === 'assistant'
 
             return (
                 <div key={message.id} className="flex items-start gap-3 text-slate-600 text-sm">
@@ -48,7 +83,12 @@ export function Chat() {
                             <AvatarImage />
                         </Avatar>
                     )}
-                    <p className="leading-relaxed dark:text-slate-400">
+                    <p className={
+                        cx(
+                            "leading-relaxed",
+                            isError ? "text-red-500" : "dark:text-slate-400"
+                        )
+                    }>
                         <span className="block font-bold text-slate-800 dark:text-slate-200">{isUser ? 'User' : 'AI'}:</span>
                         {message.content}
                     </p>
@@ -59,7 +99,9 @@ export function Chat() {
 
         <CardFooter className="flex-col items-start">
             {isLoading && <p className="ml-3">AI is typing...</p>}
-            <form className="w-full flex gap-2" onSubmit={handleSubmit}>
+            <form
+                className="w-full flex gap-2"
+                onSubmit={sendMessage}>
                 <Input placeholder="How can I help you?" value={input} onChange={handleInputChange} />
                 <Button type='submit'>Send</Button>
             </form>
